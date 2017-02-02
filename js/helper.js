a5f22d0fc4f245a49339afe7e200c24d65a7625c
Joshua Lawrence <Jlawrence124@gmail.com>
2017-02-02 17:30:06 -0500

    Resume Draft
    Draft commit 1

---
+++ js/helper.js
@@ -0,0 +1,218 @@
+var HTMLheaderName = '<h1 id="name">%data%</h1>';
+var HTMLheaderRole = '<span class="role">%data%</span><hr>';
+
+var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLlinkedIn = '<li class="flex-item"><span class="orange-text">linkedin</span><span class="white-text"><a class="a-link" href="https://www.linkedin.com/in/joshua-lawrence-9a234575">%data%</a></span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text"><a class="a-link" href="https://github.com/jlawrence124">%data%</a></span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+
+var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLwelcomeMsg = '<p class="welcome-message">%data%</p>';
+
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
+var HTMLskills = '<li class="flex-item skills-font"><p class="white-text">%data%</p></li>';
+
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<a class="ext-link" href="that">%data%</a>';
+var HTMLworkTitle = '<p class="work-title">%data%</p>';
+var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
+
+var HTMLprojectContainer = '<div class="proj-cont">'
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<p class="project-description"><br>%data%</p>';
+var HTMLprojectImage = '<img class="pro-image" src="%data%">';
+var HTMLprojectClose = '</div>'
+
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolName = '<a href="that">%data%';
+var HTMLschoolDegree = ' %data%</a>';
+var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolUrl = ""
+
+
+var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div>';
+var HTMLonlineURL = '<br><a href="%data%">%data%</a>';
+
+var internationalizeButton = '<button>Internationalize</button>';
+var googleMap = '<div id="map"></div>';
+
+
+
+$(document).ready(function() {
+    $('button').click(function() {
+        var $name = $('#name');
+        var iName = inName($name.text()) || function() {};
+        $name.html(iName);
+    });
+});
+
+
+var clickLocations = [];
+
+function logClicks(x, y) {
+    clickLocations.push({
+        x: x,
+        y: y
+    });
+    console.log('x location: ' + x + '; y location: ' + y);
+}
+
+$(document).click(function(loc) {
+    var x = loc.pageX;
+    var y = loc.pageY;
+
+    logClicks(x, y);
+});
+
+
+
+
+var map; // declares a global map variable
+
+
+
+function initializeMap() {
+
+    var locations;
+
+    var mapOptions = {
+        disableDefaultUI: true
+    };
+
+
+    map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+
+    /*
+    Returns an array of every location string from the JSONs
+    written for bio, education, and work.
+    */
+    function locationFinder() {
+
+        // initializes an empty array
+        var locations = [];
+
+        // adds the single location property from bio to the locations array
+        locations.push(bio.contacts.location);
+
+        // iterates through school locations and appends each location to
+        // the locations array.
+        education.schools.forEach(function(school) {
+            locations.push(school.location);
+        });
+
+        // iterates through work locations and appends each location to
+        // the locations array.
+        work.jobs.forEach(function(job) {
+            locations.push(job.location);
+        });
+
+        return locations;
+    }
+
+    /*
+    Reads Google Places search results to create map pins.
+    placeData is the object returned from search results containing information
+    about a single location.
+    */
+    function createMapMarker(placeData) {
+
+        // save location data from the search result object to local variables
+        var lat = placeData.geometry.location.lat(); // latitude from the place service
+        var lon = placeData.geometry.location.lng(); // longitude from the place service
+        var name = placeData.formatted_address; // name of the place from the place service
+        var bounds = window.mapBounds; // current boundaries of the map window
+
+        // marker is an object with additional data about the pin for a single location
+        var marker = new google.maps.Marker({
+            map: map,
+            position: placeData.geometry.location,
+            title: name
+        });
+
+        // helper windows that open when you click
+        // or hover over a pin on a map.
+        var infoWindow = new google.maps.InfoWindow({
+            content: name
+        });
+
+
+        google.maps.event.addListener(marker, 'click', function() {
+        });
+
+        // this is where the pin actually gets added to the map.
+        // bounds.extend() takes in a map location object
+        bounds.extend(new google.maps.LatLng(lat, lon));
+        // fit the map to the new marker
+        map.fitBounds(bounds);
+        // center the map
+        map.setCenter(bounds.getCenter());
+    }
+
+    /*
+    makes sure the search returned results for a location.
+    If so, it creates a new map marker for that location.
+    */
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            createMapMarker(results[0]);
+        }
+    }
+
+    /*
+    takes in the array of locations created by locationFinder()
+    and fires off Google place searches for each location
+    */
+    function pinPoster(locations) {
+
+        // creates a Google place search service object.
+        var service = new google.maps.places.PlacesService(map);
+
+        // Iterates through the array of locations, creates a search object for each location
+        locations.forEach(function(place) {
+            // the search request object
+            var request = {
+                query: place
+            };
+
+            // Actually searches the Google Maps API for location data and runs the callback
+            // function with the search results after each search.
+            service.textSearch(request, callback);
+        });
+    }
+
+    // Sets the boundaries of the map based on pin locations
+    window.mapBounds = new google.maps.LatLngBounds();
+
+    // locations is an array of location strings returned from locationFinder()
+    locations = locationFinder();
+
+    // creates pins on the map for each location in
+    // the locations array
+    pinPoster(locations);
+
+}
+
+
+
+// Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
+
+// Vanilla JS way to listen for resizing of the window
+// and adjust map bounds
+window.addEventListener('resize', function(e) {
+    //Make sure the map bounds get updated on page resize
+    map.fitBounds(mapBounds);
+});
---
+++ js/resumebuilder.js
@@ -0,0 +1,232 @@
+$("#mapDiv").append(googleMap);
+
+
+var bio = {
+    "name": "Joshua Lawrence",
+    "role": "Front End Web Developer",
+    "contacts": {
+        "mobile": "845-239-7867",
+        "email": "jlawrence124@gmail.com",
+        "github": "jlawrence124",
+        "linkedin": "Joshua Lawrence",
+        "location": "Charlotte, North Carolina"
+    },
+    "welcomeMessage": "Thank you for viewing my resume!  I am an energetic, optimistic, and disciplined front end developer looking to use my skils to contribute " +
+                    "to a hardworking team.  I have been working tirelessly to learn new concepts and stay up to date on all of the latest trends as an " +
+                    "active member of the front end community.",
+    "skills": [
+        "HTML 5 ●●●●○", "CSS 3 ●●●○○", "Grunt ●●○○○", "Adobe Creative Cloud ●●●○○", "Responsive Web Design  ●●●●○", "Javascript ●●●○○", "jQuery ●●●○○", "Knockout JS ●●●○○", "Python ●●○○○"
+    ],
+    "biopic": "images/me.jpg"
+};
+
+
+bio.display = function() {
+
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    var formattedNameRole = formattedName + formattedRole;
+
+
+    $("#header").prepend(formattedNameRole);
+
+
+    var contactStart = HTMLcontactGeneric;
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedGit = HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedLinkedIn = HTMLlinkedIn.replace("%data%", bio.contacts.linkedin);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    var formattedContacts = formattedMobile + formattedEmail + formattedGit + formattedLocation + formattedLinkedIn;
+
+    $("#topContacts, #footerContacts").append(formattedContacts);
+
+    var biopic = HTMLbioPic.replace("%data%", bio.biopic);
+    var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+    $("#header").append(biopic, formattedWelcome, HTMLskillsStart);
+
+    bio.skills.forEach(function(skill) {
+
+        var formattedSkill = HTMLskills.replace("%data%", skill);
+        $("#skills").append(formattedSkill);
+
+    });
+
+};
+
+bio.display();
+
+var work = {
+    "jobs": [{
+        "employer": "Geek Squad",
+        "title": "Consulation and Operations Agent",
+        "dates": "March 2016 - Present",
+        "location": "10221 Perimeter Pkwy, Charlotte, NC 28216",
+        "url": "http://www.bestbuy.com/site/electronics/geek-squad/pcmcat138100050018.c?id=pcmcat138100050018",
+        "description": "Reponsible for checking in client devices, troubleshooting software and hardware issues, " +
+                    "along with shipping and receiving client devices in and out of the precinct.  I was required to maintain " +
+                    "a comprehensive knowledge of consumer electronic devices and provide the client with a complete and prompt solution."
+
+    }, {
+        "employer": "Ruby Tuesday",
+        "title": "Host/Server",
+        "dates": "September 2013 - January 2016",
+        "location": "Selden Plaza, 289 Middle Country Rd, Selden, NY 11784",
+        "url": "http://www.rubytuesday.com/",
+        "description": "At Ruby Tuesday I always strived to provide excellent customer service with a friendly smile.  " +
+                        "I worked hard to create an inviting atmosphere for patrons, helped to foster a team centered work environment with " +
+                        "coworkers, and closely followed all health and safety guidelines."
+    }, {
+        "employer": "SUNY Orange",
+        "title": "Biological Sciences Tutor",
+        "dates": "January 2011 - December 2011",
+        "location": "115 South St, Middletown, NY 10940",
+        "url": "http://www.sunyorange.edu/las/",
+        "description": "Working as a tutor was a rewarding and interesting experience. " +
+                        "My time spent educating students helped me learn and grow as a student.  I stayed current on coursework " +
+                        "for multiple subjects and developed materials and lessons that helped students succeed."
+    }]
+};
+
+work.display = function() {
+    work.jobs.forEach(function(job) {
+        $("#workExperience").append(HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace("that", job.url).replace("%data%", job.employer);
+        var formattedWorkTitle = HTMLworkTitle.replace("%data%", job.title);
+        var formattedWorkDates = HTMLworkDates.replace("%data%", job.dates);
+        var formattedWorkLocation = HTMLworkLocation.replace("%data%", job.location);
+        var formattedWorkDescription = HTMLworkDescription.replace("%data%", job.description);
+        var formattedWork = formattedEmployer + formattedWorkTitle +
+            formattedWorkDates + formattedWorkLocation + formattedWorkDescription;
+
+        $(".work-entry:last").append(formattedWork);
+
+    });
+};
+work.display();
+
+
+var projects = {
+    "projects": [{
+        "title": "Animal Cards Markup",
+        "dates": "May 2016",
+        "description": "Created a CSS object to learn how to create a design based on a markup.",
+        "images": ["http://i.imgur.com/B2Qt7ed.png"]
+    }, {
+        "title": "Portfolio Project",
+        "dates": "June-July 2016",
+        "description": "Here I created a website based off of a rough markup.  I think really went above and beyond making it very responsive and clean.",
+        "images": ["http://i.imgur.com/SL2X3RQ.png"]
+    }, {
+        "title": "Resume Project",
+        "dates": "August 2016",
+        "description": "This resume is actually one of my projects!",
+        "images": ["http://i.imgur.com/wOkzI8e.png"]
+    }]
+};
+
+
+projects.display = function() {
+    projects.projects.forEach(function(project) {
+        {
+
+            $("#projects").append(HTMLprojectStart);
+
+            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", project.title);
+            var formattedProjectDates = HTMLprojectDates.replace("%data%", project.dates);
+            var formattedProjectDescription = HTMLprojectDescription.replace("%data%", project.description);
+
+            project.images.forEach(function(image) {
+
+                var formattedProjectImage = HTMLprojectImage.replace("%data%", image);
+                $(".project-entry:last").append(formattedProjectImage);
+            });
+
+
+            var formattedProject = formattedProjectTitle + formattedProjectDates +
+                formattedProjectDescription;
+
+            $(".project-entry:last").prepend(formattedProject);
+
+        }
+    });
+};
+
+projects.display();
+
+var education = {
+    "schools": [{
+        "name": "Stony Brook University",
+        "location": "Stony Brook, NY",
+        "degree": "-- BS: Biological Sciences - Cum Laude",
+        "majors": ["Biological Sciences"],
+        "dates": "Fall 2013 - Fall 2016",
+        "url": "http://www.stonybrook.edu/"
+    }, {
+        "name": "University of Colorado at Boulder",
+        "location": "Boulder, CO",
+        "degree": "",
+        "majors": ["Ecology and Evolutionary Biology"],
+        "dates": "Fall 2012 - Spring 2013",
+        "url": "http://www.colorado.edu/"
+    }, {
+        "name": "SUNY Orange",
+        "location": "Middletown, NY",
+        "degree": "",
+        "majors": ["Ecology and Evolutionary Biology"],
+        "dates": "Fall 2008, Spring 2011 - Spring 2012",
+        "url": "http://www.sunyorange.edu/"
+    }],
+    "onlineCourses": [{
+        "title": "Front End Nanodegree",
+        "school": "Udacity",
+        "dates": "May 2016 - Present",
+        "url": "https://www.udacity.com/nanodegree"
+    }, {
+        "title": "Python",
+        "school": "Codecademy",
+        "dates": "December 2015 - Februrary 2016",
+        "url": "https://www.codecademy.com/learn/python"
+    }]
+};
+
+
+education.display = function() {
+    education.schools.forEach(function(school) {
+
+        $("#education").append(HTMLschoolStart);
+
+        var formattedSchoolName = HTMLschoolName.replace("%data%", school.name).replace("that", school.url);
+        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", school.location);
+        var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", school.degree);
+        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", school.majors);
+        var formattedSchoolDates = HTMLschoolDates.replace("%data%", school.dates);
+        var formattedOnlineUrl = HTMLworkEmployer.replace("%data%", school.url).replace("%data%", school.url);
+
+        var formattedSchool = formattedSchoolName + formattedSchoolLocation + formattedSchoolDegree +
+            formattedSchoolDates + formattedSchoolMajor + formattedOnlineUrl;
+
+
+    $(".education-entry:last").append(formattedSchool);
+    });
+
+
+    $("#education").append(HTMLonlineClasses);
+    education.onlineCourses.forEach(function(onlineCourse) {
+
+        $("#education").append(HTMLschoolStart);
+
+        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", onlineCourse.title);
+        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", onlineCourse.school);
+        var formattedOnlineDates = HTMLonlineDates.replace("%data%", onlineCourse.dates);
+        var formattedOnlineUrl = HTMLonlineURL.replace("%data%", onlineCourse.url).replace("%data%", onlineCourse.url);
+        var formattedOnline = formattedOnlineTitle + formattedOnlineSchool + formattedOnlineDates + formattedOnlineUrl;
+
+        $(".education-entry:last").append(formattedOnline);
+
+    });
+};
+
+education.display();
